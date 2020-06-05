a3e0e44e82cb47d358c9f6776212b0a7e04cca1a
ArielCLJ <arielclj@gmail.com>
2020-06-05 12:47:59 +0800

    Revert "Fix Razer Folder name #1"
    This reverts commit a2c468550f9b879cede385c8bfc7da89b96f476f.

--- dashboard/views/error.py
+++
@@ -1,9 +0,0 @@
-from django.views           import View
-from django.shortcuts       import render, redirect
-import json
-from .                      import lang
-
-class Error( View ):
-	def get(self, request):
-		template = 'error' + lang.getTemplateByLanguage(request)
-		return render(request, template)
